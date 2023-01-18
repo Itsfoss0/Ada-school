9862a1a80bf7447d939bb0984aa359e75bfb39e4
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-18 21:25:36 +0300

    JS

--- lab/0x00-javascript_incubation/script.js
+++ lab/0x00-javascript_incubation/script.js
@@ -1,5 +1,29 @@
 let a = 1;
 let b = 2;
 
-//add 'a' and 'b' and log it out 
+//add 'a' and 'b' and log it out <-- Integer types
 console.log(a+b);
+
+// try for NaN objects
+a = "one";
+b = "two";
+
+console.log(a+b);
+
+//crate a mock object
+let mockObject = {
+    "number": 12,
+    "string": "John Doe",
+    "boolean": true
+};
+
+console.log(mockObject);
+
+// try adding different objects
+console.log(mockObject + a);
+
+// how about subtracting
+console.log(mockObject - a);
+
+// multiplication
+console.log(mockObject["number"] * 2)
