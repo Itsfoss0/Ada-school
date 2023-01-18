b1df6276a2282e7bbc882c01d477ab00610fa95a
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-18 23:14:19 +0300

    JS

--- javascript-lab/0x02-alert_prompt/script.js
+++
@@ -1,9 +0,0 @@
-const firstName = prompt("Whats your first name?", "Your name here");
-const lastName = prompt("Whats your last Name?");
-
-const fullName = firstName + " " +  lastName;
-alert(fullName);
-
-const age = prompt("How old are you?");
-const str = firstName + ", " + "you are " + age + " years old." 
-alert(str);
