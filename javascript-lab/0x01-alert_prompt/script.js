8a4a21aee83e01b0971f4c01ff950ad516cca8ab
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-20 21:29:41 +0300

    JS

--- javascript-lab/0x01-alert_prompt/script.js
+++ javascript-lab/0x01-alert_prompt/script.js
@@ -7,3 +7,14 @@ alert(fullName);
 const age = prompt("How old are you?");
 const str = firstName + ", " + "you are " + age + " years old." 
 alert(str);
+
+let step1 = "Open up your favourite Text Editor (I use vim)";
+let step2 = "Create a HTML document with a .html file extension";
+let step3 = "Type in the HTML content ( or copy and paste from ChatGPT :V ) and save the file";
+let step4 = "Open the HTML document in the browser";
+
+console.log(step1);
+console.log(step2);
+console.log(step3);
+console.log(step4);
+
