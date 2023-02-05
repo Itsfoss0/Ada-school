cd955d2465b9abbb32c615c55f62f75ae06c301d
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-05 15:36:50 +0300

    JS

--- javascript-lab/0x02-if_else_loops_funcs/script.js
+++ javascript-lab/0x02-if_else_loops_funcs/script.js
@@ -1,16 +1,35 @@
-// fullblown calculator :)
-const num1 = parseInt(prompt('First Number:'));
-const num2 = parseInt(prompt('Second Number:'));
-let result;
-const operation = prompt('What Operation would you like to do?').trim();
+while (true) {
+  var num1 = parseFloat(prompt("Enter first number:"));
+  var num2 = parseFloat(prompt("Enter second number:"));
+  var operation = prompt("Enter operation (+, -, *, /):");
 
-if (operation == '+' || operation == 'add') {
-  result = num1 + num2;
-} else if (operation == '-' || operation == 'sub') {
-  result = num1 - num2;
-} else if (operation == '*' || operation == 'mul') {
-  result = num1 * num2;
-} else {
-  result = num1 / num2;
+  if (operation === "exit") {
+    break;
+  }
+
+  if (isNaN(num1) || isNaN(num2)) {
+    alert("Invalid input. Both values must be numbers.");
+    continue;
+  }
+
+  var result;
+  switch (operation) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    default:
+      alert("Invalid operation.");
+      continue;
+  }
+
+  alert("Result: " + result);
 }
-alert(`${num1} ${operation} ${num2} = ${result}`);
