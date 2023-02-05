6c4bb768534044a59d6c2c3b7e6f353f8524ca16
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-05 23:07:40 +0300

    JS

--- javascript-lab/0x05-functions/script.js
+++ javascript-lab/0x05-functions/script.js
@@ -28,3 +28,19 @@ const reverseNum = (num) =>{
   return reversedString;
 };
 console.log(reverseNum(133));
+
+/* 
+Check whethe a given string is a palindrome or not
+*/
+
+function isPalindrome(str){
+    let reversed = "";
+    const len = str.length;
+    for (let i = len - 1; i >= 0; i--){
+        reversed += str[i];
+    }
+    return str === reversed
+}
+
+console.log(isPalindrome("dad")); // true
+console.log(isPalindrome("Doe")); // False
