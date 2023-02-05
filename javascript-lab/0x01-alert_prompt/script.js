12c5be95fdc34b81cff556940b56fc4e78d2536f
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-05 15:36:41 +0300

    JS

--- javascript-lab/0x01-alert_prompt/script.js
+++ javascript-lab/0x01-alert_prompt/script.js
@@ -1,8 +1,8 @@
-//Adding two numbers in JS
+// Adding two numbers in JS
 
-let number1 = parseInt(prompt("Enter First Number, Defualt is 0", 0));
-let number2 = parseInt(prompt("Enter second Number, Default is 0", 0));
+const number1 = parseInt(prompt('Enter First Number, Defualt is 0', 0));
+const number2 = parseInt(prompt('Enter second Number, Default is 0', 0));
 
-let sum = number1 + number2;
+const sum = number1 + number2;
 
 alert(`${number1} + ${number2} = ${sum}`);
